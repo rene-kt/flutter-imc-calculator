f94b2d01ab73b5bcf280cbf76b8e020c3be2a8aa
Renê <renejr.arraes286@gmail.com>
2020-08-31 09:56:41 -0300

    raised button and showing alert

--- lib/main.dart
+++ lib/main.dart
@@ -15,37 +15,76 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("IMC Calculator"),
-        centerTitle: true,
-        backgroundColor: Colors.deepPurple,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person_pin_circle, size: 100, color: Colors.deepPurple),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Weight',
+        appBar: AppBar(
+          title: Text("IMC Calculator"),
+          centerTitle: true,
+          backgroundColor: Colors.deepPurple,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {
+                _showDialog();
+              },
+            )
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_pin_circle,
+                  size: 100, color: Colors.deepPurple),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Weight',
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Height',
+                ),
+              ),
+              Container(
+                  height: 50.0,
+                  child: RaisedButton(
+                    onPressed: () {
+                      _showDialog();
+                    },
+                    child: Text(
+                      "Calc",
+                      style: TextStyle(color: Colors.white, fontSize: 25.0),
+                    ),
+                    color: Colors.deepPurple,
+                  ))
+            ],
+          ),
+        ));
+  }
+
+  Future<void> _showDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('AlertDialog Title'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('The result is: '),
+              ],
             ),
           ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Height',
-            ),
-          )
-        ],
-      ),
+        );
+      },
     );
+  }
+
+  double _calcImc() {
+    return 0.0;
   }
 }
